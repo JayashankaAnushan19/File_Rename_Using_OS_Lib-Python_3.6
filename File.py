2c48ee686e3207159085c65cd59be675a65259dc
JayashankaAnushan19 <jayasankaanushan199@gmail.com>
2024-08-23 21:01:08 +0530

    Updated and added author credential

---
+++ File.py
@@ -0,0 +1,22 @@
+
+### Import the OS lib
+import os 
+
+### Here as an example - below code have create a directory
+os.mkdir('Old_File_Name')
+print('Folder -- Old_File_Name  created')
+
+### This is the new file name
+New_File_name = 'This_Is_New_File'
+
+### Rename the created file
+os.rename('Old_File_Name', New_File_name)
+
+print('Folder -- New file created')
+
+
+'''
+This code written by Jayashanka Anushan 
+	Email  : jayasankaanushan199@gmail.com
+	LinkedIn : https://www.linkedin.com/in/jayashanka-anushan-011041158/
+'''
---
+++ README.md
@@ -0,0 +1,1 @@
+"Here there is a sample code to renaame the file using OS lib" 
